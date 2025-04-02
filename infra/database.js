fa1b85304dea64c4ae433756aeac1c5fbc62e7e6
Tales Eduardo <tales.e.valadares@gmail.com>
2025-04-02 19:20:30 +0000

    add a more rebust ssl configuration method

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSSLValue(),
   });
 
   try {
@@ -20,6 +20,15 @@ async function query(queryObject) {
   } finally {
     await client.end();
   }
+}
+
+function getSSLValue() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+  return process.env.NODE_ENV === "development" ? false : true;
 }
 
 export default {
