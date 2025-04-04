d2c63c42827a24e9fde1818fa8bcfb345384cba4
Tales Eduardo <tales.e.valadares@gmail.com>
2025-04-04 17:08:15 +0000

    adds `/api/vi/migrations` endpoint

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+const nextJest = require("next/jest");
+const dotenv = require("dotenv");
+
+//necessário para carregar as variaveis de ambiente
+//dentro do jest que por padrão não carregar variaveis de ambiente dentro do ambiente de test
+dotenv.config({
+  path: ".env.development",
+});
+
+//configuração para o jest saber de onde parte a raiz do projeto
+const createJestConfig = nextJest({
+  dir: ".",
+});
+
+//configuração para que o jest consiga usar o import from mais moderno
+const jestConfig = createJestConfig({
+  moduleDirectories: ["node_modules", "<rootDir>"],
+});
+
+module.exports = jestConfig;
---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,37 @@
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+import database from 'infra/database.js';
+
+export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+  
+  const defaultMigrationOptions = {
+    dbClient,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+    });
+    await dbClient.end();
+    return response.status(200).json(pendingMigrations);
+  }
+
+  if (request.method === "POST") {
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+    });
+    await dbClient.end();
+    const status = migratedMigrations.length > 0 ? 201 : 200;
+    return response.status(status).json(migratedMigrations);
+  }
+
+  await dbClient.end();
+  return response.status(405).end(); //metodo não permitido
+}
